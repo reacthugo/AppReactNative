9e07a29187d6c6b8ac20e88b0592effb4c7f7620
HugoVinicius <hugo.desenvolvimento@outlook.com>
2019-09-15 12:52:23 -0300

    Finanlizando exercicio

---
+++ api/Reddit.js
@@ -0,0 +1,13 @@
+export class Reddit {
+    static async get(tag){
+        try {
+            const response = await fetch(`https://reddit.com/r/${tag}.json`);
+            const responseJson = await response.json();
+
+            return responseJson.data.children;
+        }
+        catch (e){
+            throw e;
+        }
+    }
+}
